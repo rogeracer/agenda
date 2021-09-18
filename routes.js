58e4e22e6c78ae57bfb6cd6beb9924fe9f11d274
Rogério Amorim <rogeracer@gmail.com>
2021-09-18 18:46:19 -0300

    feat: add register

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ route.get("/", homeController.index);
 // Rotas de login
 
 route.get("/login/index", loginController.index);
+route.post("/login/register", loginController.register)
 
 module.exports = route;
 
