8782abc4325cca6062a7278cfc6d785685591f46
Rogério Amorim <rogeracer@gmail.com>
2021-09-20 15:18:42 -0300

    fix: bug no index

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const route = express.Router();
 const homeController = require("./src/controllers/homeController");
 const loginController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController");
 
 // Rotas de Home
 
@@ -13,6 +14,10 @@ route.get("/login/index", loginController.index);
 route.post("/login/register", loginController.register);
 route.post("/login/login", loginController.login);
 route.get("/login/logout", loginController.logout);
+
+// Rotas de Contato
+
+route.get("/contato/index", contatoController.index);
 
 module.exports = route;
 
